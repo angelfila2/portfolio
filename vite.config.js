ca5191c5295506b3067231c6114f5310ecdf99ce
Syafiq <syafiqwork2023@gmail.com>
2026-01-03 00:48:00 +0800

    changed portfolio name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/corgportfolio/", // 👈 MUST match your repo name
+  base: "/portfolio/", // 👈 MUST match your repo name
 });
